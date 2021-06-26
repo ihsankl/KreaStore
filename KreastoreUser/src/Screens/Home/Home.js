7deb50a37d4399b9b29c8b06a529299d39d21470
alghifarifikri <alghi77@gmail.com>
2021-06-26 20:40:12 +0700

    version 0.0.2 - push history screen

---
+++ KreastoreUser/src/Screens/Home/Home.js
@@ -0,0 +1,49 @@
+import React, {useEffect, useState} from 'react';
+import {StyleSheet, Text, View, Button, TouchableOpacity} from 'react-native';
+import firestore from '@react-native-firebase/firestore';
+
+const postRef = firestore().collection('post');
+
+const Home = props => {
+  const [data, setData] = useState([]);
+
+  console.log('props', {props});
+  useEffect(() => {
+    onInit();
+    return () => {};
+  }, []);
+
+  const onInit = async () => {
+    try {
+      const data = [];
+      const x = await postRef.get();
+      x.forEach(docs => {
+        let currentId = docs.id;
+        let appObj = {...docs.data(), ['id']: currentId};
+        data.push(appObj);
+        setData(data);
+      });
+      console.log(data);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  return (
+    <View>
+      {data.map((v, i) => {
+        return <Text>{v?.title}</Text>;
+      })}
+      <TouchableOpacity>
+        <Button
+          title="Go to History"
+          onPress={() => props.navigation.navigate('History')}
+        />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({});
