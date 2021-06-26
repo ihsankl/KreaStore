968a15b76ab4b0f105578f44db250c46072acd51
alghifarifikri <alghi77@gmail.com>
2021-06-26 20:40:12 +0700

    version 0.0.2 - push history screen

--- KreastoreUser/App.js
+++ KreastoreUser/App.js
@@ -1,22 +1,37 @@
-import React from 'react'
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
 import storage from './src/Redux/store';
-const { store, persistor } = storage();
+const {store, persistor} = storage();
 const Stack = createStackNavigator();
 
 // SCREENS HERE
-import Home from './src/Screens/Home';
+import Home from './src/Screens/Home/Home';
+import Index from './src/Screens/History/Index';
 
 const MainStackNavigator = () => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen options={{ headerShown: false }} name="Home" component={Home} />
+    <Stack.Navigator screenOptions={{headerTitleAlign: 'center'}}>
+      <Stack.Screen
+        options={{headerShown: false}}
+        name="Home"
+        component={Home}
+      />
+      <Stack.Screen
+        name="History"
+        component={Index}
+        options={{
+          title: 'Riwayat',
+          headerStyle: {
+            backgroundColor: '#F0FFFE',
+          },
+        }}
+      />
     </Stack.Navigator>
   );
-}
+};
 
 const App = () => {
   return (
@@ -27,7 +42,7 @@ const App = () => {
         </NavigationContainer>
       </PersistGate>
     </Provider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
