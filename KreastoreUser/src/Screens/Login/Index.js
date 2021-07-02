5afd026019a57301f60b50facd81f4aa19279c2f
Ihsan Kurniawan <ikurniawanlubis@gmail.com>
2021-07-02 16:59:44 +0700

    default button done

--- KreastoreUser/src/Screens/Login/Index.js
+++ KreastoreUser/src/Screens/Login/Index.js
@@ -1,62 +1,67 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, ScrollView, StyleSheet, Image, Button } from 'react-native';
+import { View, Text, ScrollView, StyleSheet, Image, Button, TouchableOpacity } from 'react-native';
 import { color } from '../../Theme/Color';
 import logo2 from '../../assets/images/logo2.png'
 import { GoogleSignin, statusCodes } from '@react-native-google-signin/google-signin';
 import { getUserData } from '../../Redux/Action/getUserData';
 import { connect } from 'react-redux'
+import KreaButton from '../../Components/KreaButton';
+import Icon from 'react-native-vector-icons/dist/Feather';
+import logo_google from '../../assets/images/logo_google.png'
 
+const Index = ({ ...props }) => {
 
-const Index = ({...props}) => {
-
-    const onLogin = async() => {
+    const onLogin = async () => {
         try {
             await GoogleSignin.hasPlayServices();
             const user = await GoogleSignin.signIn();
-            await props.dispatch(getUserData({user, isAnonymous:false}))
+            await props.dispatch(getUserData({ user, isAnonymous: false }))
         } catch (error) {
             console.log(error.message)
         }
     }
-    
-    const onAnonLogin = async() => {
+
+    const onAnonLogin = async () => {
         try {
-            await props.dispatch(getUserData({...props.userData.data, isAnonymous:true}))
+            await props.dispatch(getUserData({ ...props.userData.data, isAnonymous: true }))
         } catch (error) {
             console.log(error.message)
         }
     }
-    
+
     return (
         <ScrollView contentContainerStyle={styles.container}>
             <Image source={logo2} style={styles.logo} resizeMode="cover" />
-            <Text>KreaStore</Text>
-            <Text>Selamat datang di KreaStore, yuk cari donatur untuk mendanai karyamu.</Text>
+            <Text style={{ fontFamily: 'Poppins-Bold', fontSize: 36, color: '#474747' }}>KreaStore</Text>
+            <Text style={{ textAlign: 'center' }}>Selamat datang di KreaStore, yuk cari donatur untuk mendanai karyamu.</Text>
 
-            <Button onPress={onAnonLogin} title="Masuk Tanpa Login" />
-            <Button onPress={onLogin} title="Login" />
+            <KreaButton onPress={onAnonLogin} btnStyle={{marginTop:35}} text="Masuk Tanpa Login" btnColor={color.secondary}/>
+            <KreaButton onPress={onLogin} textStyle={{marginLeft:4}} text="Masuk Dengan Google" btnStyle={{marginTop:15}} btnColor={color.secondary}>
+                <Image source={logo_google} style={{height:20, width:20}} resizeMode="cover" />
+            </KreaButton>
+
         </ScrollView>
     );
 }
 
 const mapStateToProps = state => {
     return {
-      userData: state.userData,
-      alert: state.alert,
+        userData: state.userData,
+        alert: state.alert,
     }
-  }
-  
-  export default connect(mapStateToProps)(Index);
+}
+
+export default connect(mapStateToProps)(Index);
 
 const styles = StyleSheet.create({
     container: {
         flexGrow: 1,
-        backgroundColor: color.accent2,
+        backgroundColor: color.accent3,
         justifyContent: 'center',
         alignItems: 'center'
     },
-    logo:{
-        height:200,
-        width:200,
+    logo: {
+        height: 200,
+        width: 200,
     }
 })
